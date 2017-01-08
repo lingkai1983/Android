b710ee9d6ec20c0d1fc91b60afa5739dd8f9b8d6
Zuo, Lingkai <lingkai.zuo@gatech.edu>
2017-01-08 13:47:07 -0800

    update
    1. update for 3B

--- Lesson2_UserInput/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson2_UserInput/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,12 +6,17 @@
  */
 package com.example.android.justjava;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+
+import static android.R.attr.checked;
 
 /**
  * This app displays an order form to order coffee.
@@ -26,42 +31,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+    /**
+    * @param number
+    * @param  creamChecked
+     */
+
+    private  String createOrderSummary(int number, boolean creamChecked, boolean chocolateChcecked){
+        String s = "Name: Lingkai Zuo\n";
+        s += "Added Whipped Cream? " +  creamChecked + "\n";
+        s += "Adeed Chocolate? " + chocolateChcecked + "\n";
+        s += "Quantity: " + quantity +"\n";
+        s += "Total: $" + number +"\n";
+        s += "Thank you!";
+        return s;
+        }
+
+    /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + quantity*5 +"\nThank you!";
+        int totalPrice = calculatePrice();
+
+        CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox_whipped_cream);
+        boolean creamChecked = checkbox.isChecked();
+
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.checkbox_chocolate);
+        boolean  chocolateChecked = chocolateCheckbox.isChecked();
+
+        String priceMessage = createOrderSummary(totalPrice,creamChecked,chocolateChecked);
         displayMessage(priceMessage);
     }
 
     public void increment(View view){
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view){
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 }
