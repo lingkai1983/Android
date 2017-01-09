770b056993bd56cacb08d7c5ed0579aaef5b5010
Zuo, Lingkai <lingkai.zuo@gatech.edu>
2017-01-08 16:37:08 -0800

    update for style

--- Lesson2_UserInput/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson2_UserInput/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,23 +6,24 @@
  */
 package com.example.android.justjava;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.text.NumberFormat;
-
-import static android.R.attr.checked;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +36,17 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param quantity is the number of cups of coffee ordered
      */
-    private int calculatePrice() {
+    private int calculatePrice(boolean hasCream, boolean hasChocolate) {
         int price = quantity * 5;
+
+        if(hasCream){
+            price = price + quantity*1;
+        }
+
+        if (hasChocolate) {
+            price = price + quantity*2;
+        }
+
         return price;
     }
 
@@ -45,8 +55,9 @@ public class MainActivity extends AppCompatActivity {
     * @param  creamChecked
      */
 
-    private  String createOrderSummary(int number, boolean creamChecked, boolean chocolateChcecked){
-        String s = "Name: Lingkai Zuo\n";
+    private  String createOrderSummary(int number, boolean creamChecked, boolean chocolateChcecked,
+                                       String userName){
+        String s = "Name: " +userName + "\n";
         s += "Added Whipped Cream? " +  creamChecked + "\n";
         s += "Adeed Chocolate? " + chocolateChcecked + "\n";
         s += "Quantity: " + quantity +"\n";
@@ -59,25 +70,58 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int totalPrice = calculatePrice();
-
         CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox_whipped_cream);
         boolean creamChecked = checkbox.isChecked();
 
         CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.checkbox_chocolate);
         boolean  chocolateChecked = chocolateCheckbox.isChecked();
 
-        String priceMessage = createOrderSummary(totalPrice,creamChecked,chocolateChecked);
-        displayMessage(priceMessage);
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String userName = nameField.getText().toString();
+
+        int totalPrice = calculatePrice(creamChecked, chocolateChecked);
+
+        // generate the message want to send
+        String priceMessage = createOrderSummary(totalPrice,creamChecked,chocolateChecked,userName);
+
+        // create an intent to send email
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Jave Order to: " + userName);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        // displayMessage(priceMessage);
     }
 
     public void increment(View view){
-        quantity = quantity + 1;
+        if (quantity <= 9) {
+            quantity = quantity + 1;
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "You can only order up to 10 coffees!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
         displayQuantity(quantity);
     }
 
     public void decrement(View view){
-        quantity = quantity - 1;
+        if (quantity >=2) {
+            quantity = quantity - 1;
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "You need to order at least 1 cup of coffee";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.show();
+        }
+
         displayQuantity(quantity);
     }
 
@@ -93,9 +137,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
+/*    private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
-    }
+    }*/
 
 }
