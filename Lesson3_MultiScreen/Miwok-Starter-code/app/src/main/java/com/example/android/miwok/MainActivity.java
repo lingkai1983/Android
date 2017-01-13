2ab4acde8fa43b92c10c1af5aae58c44ddc0cc09
Zuo, Lingkai <lingkai.zuo@gatech.edu>
2017-01-13 10:14:14 -0800

    update for arrayList inside number activity

--- Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +29,50 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // There are four listeners Color, Family, Number and Phrase
+
+        // Number and color using the style that onclick method defined in a seperate java files
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        numbers.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent numberIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numberIntent);
+            }
+        }
+        );
+
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+        colors.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        }
+        );
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phraseIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phraseIntent);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+        family.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        }
+
+        );
     }
 
-    public void openNumbersList(View view){
-        Intent intent = new Intent(this,NumbersActivity.class);
-        startActivity(intent);
-    }
 }
--- Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+        String[] words = {"One", "Two", "Three", "Four", "Five",
+                "Six", "Seven", "Eight", "Nine", "Ten"};
+        for (int i = 0; i < words.length; i++) {
+            Log.v("Number Activity", "Word at Index " + i + " is " + words[i]);
+        }
+
     }
 }
