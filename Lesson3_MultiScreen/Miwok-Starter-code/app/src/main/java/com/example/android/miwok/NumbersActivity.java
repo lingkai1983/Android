90a793149bd3dd3017c4825e37d9c1c6b35ea0a9
Zuo, Lingkai <lingkai.zuo@gatech.edu>
2017-01-14 15:40:41 -0800

    Revert "update for complete the words"
    This reverts commit a074d9e4cd0157e6962ee3fcb522c754736fe250.

--- Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -27,7 +28,7 @@ public class NumbersActivity extends AppCompatActivity {
 
         WordAdapter itemsAdapter = new WordAdapter(this, words);
 
-         ListView listView = (ListView) findViewById(R.id.number_list);
+         ListView listView = (ListView) findViewById(R.id.list);
 
          listView.setAdapter(itemsAdapter);
 
--- Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ Lesson3_MultiScreen/Miwok-Starter-code/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,10 +1,7 @@
 package com.example.android.miwok;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.os.Bundle;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -12,22 +9,5 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrase);
-
-        ArrayList<Word> phrases = new ArrayList<Word> ();
-
-        phrases.add(new Word("Where are you going?","minto wuksus"));
-        phrases.add(new Word("What is your name?","tinna oyaase'na"));
-        phrases.add(new Word("My name is...","oyaaset..."));
-        phrases.add(new Word("How are you feeling?","michaksas?"));
-        phrases.add(new Word("I'm feeling good.","kuchi achit"));
-        phrases.add(new Word("Are you coming?","aanas'aa?"));
-        phrases.add(new Word("Yes, I'm coming","haa'aanam"));
-        phrases.add(new Word("I'm coming","aanam"));
-        phrases.add(new Word("Let's go","yoowutis"));
-        phrases.add(new Word("Come here","aani'nem"));
-
-        WordAdapter itemAdapter = new WordAdapter(this, phrases);
-        ListView    phraseView = (ListView) findViewById(R.id.phrase_list);
-        phraseView.setAdapter(itemAdapter);
     }
 }
