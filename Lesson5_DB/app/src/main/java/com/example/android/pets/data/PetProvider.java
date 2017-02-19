056fdf3db608a2f74fecdedfeccc4c264f9a337d
Lingkai Zuo <lingkai.zuo@gatech.edu>
2017-02-18 21:48:36 -0800

    update for sanitycheck

--- Lesson5_DB/app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ Lesson5_DB/app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -7,6 +7,7 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 /**
  * {@link ContentProvider} for Pets app.
@@ -106,7 +107,55 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return insertPet(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Insert a pet into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertPet(Uri uri, ContentValues values) {
+        String name = values.getAsString(PetContract.PetEntry.COLUMN_PET_NAME);
+        String breed = values.getAsString(PetContract.PetEntry.COLUMN_PET_BREED);
+        int    weight = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+        String gender = values.getAsString(PetContract.PetEntry.COLUMN_PET_GENDER);
+
+        if (name == null) {
+            throw new IllegalArgumentException("Pet requires a name");
+        }
+        // TODO: Finish sanity checking the rest of the attributes in ContentValues
+        if (breed == null){
+            throw  new IllegalArgumentException("Pet requires a breed");
+        }
+        if (weight <= 0 ){
+            throw  new IllegalArgumentException("Pet requires a weight");
+        }
+        if (gender == null){
+            throw  new IllegalArgumentException("Pet requires a gender");
+        }
+
+
+
+
+        // TODO: Insert a new pet into the pets database table with the given ContentValues
+        // create a db that code could write
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        long id = db.insert(PetContract.PetEntry.TABLE_NAME,null,values);
+
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+
+        if (id == -1){
+            Log.e(LOG_TAG, "Fail to insert row for " + uri);
+        }
+        return ContentUris.withAppendedId(uri, id);
     }
 
     /**
