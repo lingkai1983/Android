a60fd497785d2e9c19e4347017eb4f2980287ca8
Lingkai Zuo <lingkai.zuo@gatech.edu>
2017-02-19 21:25:49 -0800

    fix error

--- Lesson5_DB/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Lesson5_DB/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -15,21 +15,19 @@
  */
 package com.example.android.pets;
 
-import android.content.ContentUris;
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.android.pets.data.PetContract.PetEntry;
